72245e1bb9c34c8771c58510f2659725d7d81811
Yau <yhauxell@gmail.com>
2025-12-01 16:10:44 +0100

    feat: update app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,21 +87,8 @@ export const metadata: Metadata = {
   },
   category: "technology",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/icon.svg",
+    apple: "/icon.svg",
   },
   verification: {
     // Add your verification codes here when available
